eac0dd0a2dc695517ef7b9498d5782b12b346269
Geison Biazus <geisonbiazus@gmail.com>
2018-01-31 11:07:22 -0200

    remove react-server

--- react-server/src/app/list_item.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class ListItem extends Component {
-  render() {
-    return (
-      <li>List Item {this.props.index}</li>
-    );
-  }
-}
